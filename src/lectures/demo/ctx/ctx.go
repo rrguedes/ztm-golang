fdb24ffc93d422f545675484e9978651007f876e
Rafael Guedes <rafaelguedes@gmail.com>
2023-03-05 20:39:40 -0300

    Context e generators

--- src/lectures/demo/ctx/ctx.go
+++ src/lectures/demo/ctx/ctx.go
@@ -1,5 +1,50 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"time"
+)
 
+func sampleOperation(ctx context.Context, msg string, msDelay time.Duration) <-chan string {
+	out := make(chan string)
+
+	go func() {
+		for {
+			select {
+			case <-time.After(msDelay * time.Millisecond):
+				out <- fmt.Sprintf("%v Operation Completed", msg)
+				return
+			case <-ctx.Done():
+				out <- fmt.Sprintf("%v Abortado", msg)
+				return
+			}
+		}
+	}()
+	return out
+}
+
+// Usar contet quando trabalhar com multiplas goroutines, pois é possível cancelar todas usando uma única chamada do cancelamento do contexto.
+func main() {
+	ctx := context.Background()
+	ctx, cancelCtx := context.WithCancel(ctx)
+
+	webServer := sampleOperation(ctx, "Webserver", 200)
+	microservice := sampleOperation(ctx, "Microservice", 500)
+	database := sampleOperation(ctx, "Database", 900)
+Mainloop:
+	for {
+		select {
+		case val := <-webServer:
+			fmt.Println(val)
+		case val := <-microservice:
+			fmt.Println(val)
+			fmt.Println("Cancel Context")
+			cancelCtx()
+			break Mainloop
+		case val := <-database:
+			fmt.Println(val)
+		}
+	}
+	fmt.Println(<-database)
 }
--- src/lectures/demo/generator/generator.go
+++ src/lectures/demo/generator/generator.go
@@ -1,4 +1,59 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func generateRandIntn(count, min, max int) <-chan int {
+	out := make(chan int, 1)
+
+	go func() {
+		for i := 0; i < count; i++ {
+			out <- rand.Intn(max-min+1) + min
+		}
+		close(out)
+	}()
+	return out
+}
+
+func generateRandInt(min, max int) <-chan int {
+	out := make(chan int, 3)
+
+	go func() {
+		for {
+			out <- rand.Intn(max-min+1) + min
+		}
+	}()
+	return out
+}
+
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	randInt := generateRandInt(1, 100)
+
+	fmt.Println("Generating Inifite Random Numbers")
+	fmt.Println(<-randInt)
+	fmt.Println(<-randInt)
+	fmt.Println(<-randInt)
+	fmt.Println(<-randInt)
+
+	randIntCount := generateRandIntn(2, 1, 10)
+	fmt.Println("Generating 2 Random Numbers")
+	for i := range randIntCount {
+		fmt.Println(i)
+	}
+
+	fmt.Println("Generating 3 Random Numbers")
+	randIntCount2 := generateRandIntn(3, 1, 10)
+	// Padrao util para quando buscar dados de um canal fechado retorna valores padrao
+	for {
+		n, open := <-randIntCount2
+		if !open {
+			break
+		}
+		fmt.Println(n)
+	}
 }
