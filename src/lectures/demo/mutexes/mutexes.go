14798dbb1795b4f842c6dfb3225523a494e54dda
Rafael Guedes <rafaelguedes@gmail.com>
2023-03-05 15:11:04 -0300

    Mutex, waitgroups and synchronization exercise

--- src/lectures/demo/mutexes/mutexes.go
+++ src/lectures/demo/mutexes/mutexes.go
@@ -3,8 +3,23 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
+
+type Hits struct {
+	count int
+	sync.Mutex
+}
+
+func serve(wg *sync.WaitGroup, hits *Hits, iteration int) {
+	wait()
+	hits.Lock()
+	defer hits.Unlock()
+	defer wg.Done()
+	hits.count += 1
+	fmt.Println("Served iteration", iteration)
+}
 
 func wait() {
 	time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
@@ -12,4 +27,17 @@ func wait() {
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
+	var wg sync.WaitGroup
+	hitsCounter := Hits{}
+	for i := 0; i < 20; i++ {
+		iteration := i
+		wg.Add(1)
+		go serve(&wg, &hitsCounter, iteration)
+	}
+	fmt.Println("Waiting for goroutines")
+	wg.Wait()
+	hitsCounter.Lock()
+	totalHits := hitsCounter.count
+	hitsCounter.Unlock()
+	fmt.Println("Total hits:", totalHits)
 }
--- src/lectures/exercise/sync/sync.go
+++ src/lectures/exercise/sync/sync.go
@@ -19,7 +19,66 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"sync"
+	"time"
 )
 
-func main() {}
+// Estrutura de dados que carrega o Mutex e o contador
+type LetterCounter struct {
+	totalLetters int
+	sync.Mutex
+}
+
+func wait() {
+	time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+}
+
+func countLetters(wg *sync.WaitGroup, letterCounter *LetterCounter, currentWord string) {
+	wait()
+	letterCounter.Lock()
+	defer letterCounter.Unlock()
+	defer wg.Done()
+	letterCounter.totalLetters += len(currentWord)
+}
+
+func main() {
+	//--Summary:
+	//  Create a program that can read text from standard input and count the
+	//  number of letters present in the input.
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("-- Word Counter: Type some sentences so that I can count the words --")
+	data, _ := reader.ReadString('\n')
+	var wg sync.WaitGroup
+	listOfWords := strings.Split(data, " ")
+	letterCounter := LetterCounter{}
+	for i := 0; i < len(listOfWords); i++ {
+		currentWord := listOfWords[i]
+		wg.Add(1)
+		go countLetters(&wg, &letterCounter, currentWord)
+	}
+	fmt.Println("Waiting for goroutines")
+	wg.Wait()
+	letterCounter.Lock()
+	totalLetters := letterCounter.totalLetters
+	letterCounter.Unlock()
+	fmt.Println("Total letters:", totalLetters)
+	//--Requirements:
+	//* Count the total number of letters in any chosen input
+	//* The input must be supplied from standard input
+	//* Input analysis must occur per-word, and each word must be analyzed
+	//  within a goroutine
+
+	//* When the program finishes, display the total number of letters counted
+	//
+	//--Notes:
+	//* Use CTRL+D (Mac/Linux) or CTRL+Z (Windows) to signal EOF, if manually
+	//  entering data
+	//* Use `cat FILE | go run ./exercise/sync` to analyze a file
+	//* Use any synchronization techniques to implement the program:
+	//  - Channels / mutexes / wait groups
+}
